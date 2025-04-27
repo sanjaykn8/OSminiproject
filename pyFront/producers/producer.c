21bb2b622ec5cdb5cb72021102451d692d950796
Sanjay <nisanir8@gmail.com>
2025-04-27 20:20:30 +0530

    Update producer.c

--- pyFront/producers/producer.c
+++ pyFront/producers/producer.c
@@ -7,10 +7,10 @@
 #include <errno.h>
 #include <signal.h>
 
-// PID file path
+// PID file path and pipe path
 #define PID_FILE "/tmp/producer_pids"
+#define PIPE_PATH "/tmp/producer_pipe"
 
-// Function to clean up the PID from the PID file
 void remove_pid(pid_t pid) {
     FILE *fp = fopen(PID_FILE, "r+");
     if (!fp) {
@@ -18,25 +18,21 @@ void remove_pid(pid_t pid) {
         return;
     }
 
-    // Temporary buffer to store all PIDs except the current one
-    char buffer[1024];
+    char buffer[4096];
     size_t size = 0;
     pid_t temp_pid;
 
-    // Read all PIDs and rebuild the file without the current PID
     while (fscanf(fp, "%d\n", &temp_pid) == 1) {
         if (temp_pid != pid) {
             size += snprintf(buffer + size, sizeof(buffer) - size, "%d\n", temp_pid);
         }
     }
 
-    // Truncate and overwrite the PID file with the updated content
     freopen(PID_FILE, "w", fp);
     fwrite(buffer, 1, size, fp);
     fclose(fp);
 }
 
-// Signal handler for cleanup on termination
 void cleanup(int signum) {
     pid_t pid = getpid();
     printf("Cleaning up PID %d...\n", pid);
@@ -47,11 +43,9 @@ void cleanup(int signum) {
 int main(int argc, char *argv[]) {
     const char *name = argc > 1 ? argv[1] : "Producer";
 
-    // Register signal handler for termination (SIGTERM, SIGINT, etc.)
     signal(SIGTERM, cleanup);
     signal(SIGINT, cleanup);
 
-    // Register own PID in the PID file
     FILE *fp = fopen(PID_FILE, "a");
     if (!fp) {
         fprintf(stderr, "Failed to open PID file %s: %s\n", PID_FILE, strerror(errno));
@@ -61,13 +55,22 @@ int main(int argc, char *argv[]) {
     fprintf(fp, "%d\n", pid);
     fclose(fp);
 
-    // Simulate work
-    while (1) {
-        printf("%s (PID %d): working...\n", name, pid);
-        fflush(stdout);
+    // Open the pipe
+    int pipe_fd;
+    while ((pipe_fd = open(PIPE_PATH, O_WRONLY)) == -1) {
+        perror("Waiting for manager to create pipe");
         sleep(1);
     }
 
+    int counter = 0;
+    while (1) {
+        dprintf(pipe_fd, "%d\n", counter);  // send number to pipe
+        printf("%s (PID %d): running (%d)\n", name, pid, counter);
+        fflush(stdout);
+        counter++;
+        sleep(1);
+    }
+
+    close(pipe_fd);
     return 0;
 }
-
